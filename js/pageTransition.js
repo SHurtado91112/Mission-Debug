b3ca7e1474a89867f4ef6aa2a6c5de2e4227bf9c
RenzoJRodriguez <Renzo.J.Rodriguez03@gmail.com>
2017-01-21 13:54:58 -0500

    SwampHacks 2017

---
+++ js/pageTransition.js
@@ -0,0 +1,13 @@
+
+$(document).ready(
+     function() {
+
+          // Function transitions page from main menu to another page
+          $("#startButton").click(
+               function(){
+               $("#body").fadeOut();
+               }
+          );
+
+
+});
---
+++ js/typingText.js
@@ -0,0 +1,17 @@
+
+$(document).ready(
+     function() {
+
+      $("#appName").typed({
+
+        strings: ["Name of App Here"],
+        typeSpeed: 60,
+     //    startDelay: 2000,
+        showCursor: false,
+          });
+
+
+
+
+
+});
